4841e880970fa3df71e6a2363cfd76f43e981a61
Uwais Shaikh <122542988+shaik-mohammed-uwais@users.noreply.github.com>
2025-09-26 22:33:32 +0530

    Update healper.js

--- healperFunc/healper.js
+++ healperFunc/healper.js
@@ -1,7 +1,15 @@
-export default function healper(start, end, step = 1) {
+export function healper(start, end, step = 1) {
   const result = [];
   for (let i = start; i <= end; i = i + step) {
     result.push(i);
   }
   return result;
+}
+
+export function checkvalid(num, start, end, field) {
+  if (num < start || num > end) {
+    throw new Error(
+      `value of ${field} should be with in range ${start} and ${end}`
+    );
+  }
 }
