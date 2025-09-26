f6a5f75c8bdb91b2f6bdb1b0feb4280cc07df3a4
Uwais Shaikh <122542988+shaik-mohammed-uwais@users.noreply.github.com>
2025-09-26 22:33:11 +0530

    Update evaluatecron.js

--- evaluateCron/evaluatecron.js
+++ evaluateCron/evaluatecron.js
@@ -1,6 +1,6 @@
-import healper from "../healperFunc/healper.js";
+import { checkvalid, healper } from "../healperFunc/healper.js";
 
-const range = {
+export const range = {
   minute: [0, 59],
   hour: [0, 23],
   "day of month": [1, 31],
@@ -18,11 +18,14 @@ export default function evaluatecron(field, val) {
 
   if (val.includes(",")) {
     let ranesvalues = val.split(",").map(Number);
+    ranesvalues.forEach((v) => checkvalid(v, min, max, field));
     return ranesvalues;
   }
 
   if (val.includes("-")) {
     let [start, end] = val.split("-").map(Number);
+    checkvalid(start, min, max, field);
+    checkvalid(end, min, max, field);
     let range = healper(start, end);
     return range;
   }
@@ -36,8 +39,13 @@ export default function evaluatecron(field, val) {
     }
 
     let [start, end] = left.split("-").map(Number);
+    checkvalid(start, min, max, field);
+    checkvalid(end, min, max, field);
     let res = healper(start, end, right);
     return res;
   }
-  return [Number(val)];
+
+  let num = Number(val);
+  checkvalid(num, min, max, field);
+  return [num];
 }
